282357028fa572c43dfb8d9bbe8d1a0c4ab24612
Anahita Tiwary <anahitatiwary@gmail.com>
2024-02-13 22:39:13 +0530

    sheet creation

--- sheet-handling.js
+++ sheet-handling.js
@@ -1,96 +1,139 @@
+let activeSheetColor  ="#ced6e0";
+let sheetsFolderCont = document.querySelector(".sheets-folder-cont");
+let addSheetBtn = document.querySelector(".sheet-add-icon");
 
-
-let addSheetBtn = document.querySelector('.sheet-add-icon');
-addSheetBtn.addEventListener("click", ()=>{
+addSheetBtn.addEventListener("click", (e) => {
     let sheet = document.createElement("div");
     sheet.setAttribute("class", "sheet-folder");
 
     let allSheetFolders = document.querySelectorAll(".sheet-folder");
     sheet.setAttribute("id", allSheetFolders.length);
+
     sheet.innerHTML = `
-    <div class="sheet-content">Sheet ${allSheetFolders.length + 1}</div>`;
+        <div class="sheet-content">Sheet ${allSheetFolders.length + 1}</div>
+    `;
 
-let sheetsFolderContainer = document.querySelector('.sheets-folder-container');
+    sheetsFolderCont.appendChild(sheet);
+    sheet.scrollIntoView();
 
-sheetsFolderContainer.appendChild(sheet);
-createSheetDB();
-createGraphComponentMatrix();
-handleSheetActiveness(sheet);
+    // DB
+    createSheetDB();
+    createGraphComponentMatrix();
+    handleSheetActiveness(sheet);
+    handleSheetRemoval(sheet);
+    sheet.click();
 })
 
-function createSheetDB(){
-    let collectedSheedDb = [];
-    let sheetDb=[];
-    for (let i = 0; i < rows; i++) {
-        let sheetRow = [];
-      for (let j = 0; j < cols; j++) {
-        let cellProp ={
-           bold: false,
-           italic: false,
-           underline: false,
-           alignment : 'left',
-           fontFamily: "monospace",
-           fontSize: 14,
-           fontColor: "#000000",
-           value: '',
-           formula: '',
-           BGColor: "#f2f1ef",  // just for indication purpose default value 
-           children:[],
+function handleSheetRemoval(sheet) {
+    sheet.addEventListener("mousedown", (e) => {
+        // Right click
+        if (e.button !== 2) return;
+
+        let allSheetFolders = document.querySelectorAll(".sheet-folder");
+        if (allSheetFolders.length === 1) {
+            alert("You need to have atleast one sheet!!");
+            return;
         }
-        sheetRow.push(cellProp);
-      }  
-      sheetDb.push(sheetRow);  
-    } 
-    collectedSheedDb.push(sheetDb);
+
+        let response = confirm("Your sheet will be removed permanently, Are you sure?");
+        if (response === false) return;
+
+        let sheetIdx = Number(sheet.getAttribute("id"));
+        // DB
+        collectedSheetDB.splice(sheetIdx, 1);
+        collectedGraphComponent.splice(sheetIdx, 1);
+        // UI
+        handleSheetUIRemoval(sheet)
+
+        // By default DB to sheet 1 (active)
+        sheetDB = collectedSheetDB[0];
+        graphComponentMatrix = collectedGraphComponent[0];
+        handleSheetProperties();
+    })
 }
 
-
-function createGraphComponentMatrix(){
-    // Storage -> 2D array (Basic needed)
-let collectedGraphComponent = [];
-let graphComponentMatrix = [];
-
-for (let i = 0; i < rows; i++) {
-    let row = [];
-    for (let j = 0; j < cols; j++) {
-        // Why array -> More than 1 child relation(dependency)
-        row.push([]);
+function handleSheetUIRemoval(sheet) {
+    sheet.remove();
+    let allSheetFolders = document.querySelectorAll(".sheet-folder");
+    for (let i = 0;i < allSheetFolders.length;i++) {
+        allSheetFolders[i].setAttribute("id", i);
+        let sheetContent = allSheetFolders[i].querySelector(".sheet-content");
+        sheetContent.innerText = `Sheet ${i+1}`;
+        allSheetFolders[i].style.backgroundColor = "transparent";
     }
-    graphComponentMatrix.push(row);
-}
-collectedGraphComponent.push(graphComponentMatrix);
-}
 
-function handleSheetActiveness(){
-   sheet.addEventListener('click', ()=>{
-    let sheetIdx = Number(sheet.getAttribute("id"));
-    handleSheetDb(sheetIdx);
-    handleSheetProperties();
-   })
-}  
-
-function handleSheetDb(){
-sheetDb = collectedSheedDb[sheetIdx];
-graphComponentMatrix = collectedGraphComponent[sheetIdx];
+    allSheetFolders[0].style.backgroundColor = activeSheetColor;
 }
 
-function handleSheetProperties(){
+function handleSheetDB(sheetIdx) {
+    sheetDB = collectedSheetDB[sheetIdx];
+    graphComponentMatrix = collectedGraphComponent[sheetIdx];
+}
+
+function handleSheetProperties() {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            let cell = document.createElement('div');
+            let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
             cell.click();
         }
-        
     }
-    let firstCell = document.querySelector('.cell');
-firstCell.click();
+    // By default click on first cell via DOM
+    let firstCell = document.querySelector(".cell");
+    firstCell.click();
 }
 
-function handleSheetUI(sheet){
+function handleSheetUI(sheet) {
     let allSheetFolders = document.querySelectorAll(".sheet-folder");
-   for(let i = 0; i<allSheetFolders.length; i++){
-    allSheetFolders[i].style.backgroundColor = 'transparent';
-   }
-   sheet.style.backgroundColor ="#ced6e0";
+    for (let i = 0; i < allSheetFolders.length; i++) {
+        allSheetFolders[i].style.backgroundColor = "transparent";
+    }
+    sheet.style.backgroundColor = activeSheetColor;
 }
 
+function handleSheetActiveness(sheet) {
+    sheet.addEventListener("click", (e) => {
+        let sheetIdx = Number(sheet.getAttribute("id"));
+        handleSheetDB(sheetIdx);
+        handleSheetProperties();
+        handleSheetUI(sheet);
+        console.log(sheetDB);
+    })
+}
+
+function createSheetDB() {
+    let sheetDB = [];
+    for (let i = 0; i < rows; i++) {
+        let sheetRow = [];
+        for (let j = 0; j < cols; j++) {
+            let cellProp = {
+                bold: false,
+                italic: false,
+                underline: false,
+                alignment: "left",
+                fontFamily: "monospace",
+                fontSize: "14",
+                fontColor: "#000000",
+                BGcolor: "#000000",  // Just for indication purpose,
+                value: "",
+                formula: "",
+                children: [],
+            }
+            sheetRow.push(cellProp);
+        }
+        sheetDB.push(sheetRow);
+    }
+    collectedSheetDB.push(sheetDB);
+}
+
+function createGraphComponentMatrix() {
+    let graphComponentMatrix = [];
+    for (let i = 0; i < rows; i++) {
+        let row = [];
+        for (let j = 0; j < cols; j++) {
+            // Why array -> More than 1 child relation(dependency)
+            row.push([]);
+        }
+        graphComponentMatrix.push(row);
+    }
+    collectedGraphComponent.push(graphComponentMatrix);
+}
