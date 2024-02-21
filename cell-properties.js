6ee52a90ae52f34ac2793e2ff0d0cce616e5f5e0
Anahita Tiwary <anahitatiwary@gmail.com>
2024-02-22 00:09:48 +0530

    range selection

--- cell-properties.js
+++ cell-properties.js
@@ -7,26 +7,26 @@ let collectedGraphComponent= [];
   addSheetBtn.click();
   handleSheetProperties();
 }
-// for (let i = 0; i < rows; i++) {
-//     let sheetRow = [];
-//   for (let j = 0; j < cols; j++) {
-//     let cellProp ={
-//        bold: false,
-//        italic: false,
-//        underline: false,
-//        alignment : 'left',
-//        fontFamily: "monospace",
-//        fontSize: 14,
-//        fontColor: "#000000",
-//        value: '',
-//        formula: '',
-//        BGColor: "#f2f1ef",  // just for indication purpose default value 
-//        children:[],
-//     }
-//     sheetRow.push(cellProp);
-//   }  
-//   sheetDb.push(sheetRow);  
-// }
+for (let i = 0; i < rows; i++) {
+    let sheetRow = [];
+  for (let j = 0; j < cols; j++) {
+    let cellProp ={
+       bold: false,
+       italic: false,
+       underline: false,
+       alignment : 'left',
+       fontFamily: "monospace",
+       fontSize: 14,
+       fontColor: "#000000",
+       value: '',
+       formula: '',
+       BGColor: "#f2f1ef",  // just for indication purpose default value 
+       children:[],
+    }
+    sheetRow.push(cellProp);
+  }  
+  sheetDb.push(sheetRow);  
+}
 
 
 //Selectors for cell property
