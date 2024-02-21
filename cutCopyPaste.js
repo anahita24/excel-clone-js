8c039cb78b365f0324b7ff70d2c6281293496df3
Anahita Tiwary <anahitatiwary@gmail.com>
2024-02-22 00:03:15 +0530

    range selection

---
+++ cutCopyPaste.js
@@ -0,0 +1,31 @@
+let ctrlKey
+
+document.addEventListener("keydown", (e)=>{
+    ctrlKey = e.ctrlKey;
+})
+
+document.addEventListener("keyup", (e)=>{
+    ctrlKey = e.ctrlKey;
+})
+
+for(let i=0; i< rows; i++){
+    for(let j=0; j<cols; j++){
+        let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
+    }
+}
+let rangeStorage = [];
+
+function handleSelectedCell(cell){
+    cell.addEventListener("click", (e)=>{
+        // Select cells range work 
+        if(!ctrlKey) return;
+        if(rangeStorage.length >= 2) return;
+        
+        //UI 
+        cell.style.border = "3px solid #218c74";
+
+        let rid = Number(cell.getAttribute("rid"));
+        let cid = Number(cell.getAttribute("cid"));
+        rangeStorage.push([rid, cid]);
+    })
+}
