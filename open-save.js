2cbb55756cfa5b26d5e2f8d0b2d6e2cd5c3c110c
Anahita Tiwary <anahitatiwary@gmail.com>
2024-02-27 23:45:44 +0530

    download

--- open-save.js
+++ open-save.js
@@ -1,2 +1,9 @@
 let downloadBtn = document.querySelector(".download");
 let openbtn = document.querySelector(".open");
+
+downloadBtn.addEventListener("click", (e)=>{
+  let jsonData = JSON.stringify([sheetDb , graphComponentMatrix]);
+  let file = new Blob([jsonData], {
+    type: "application/json"
+  })
+})
