f3cb1aef8d38ac5b8e4c8e0707669babd2c23a4a
Anahita Tiwary <anahitatiwary@gmail.com>
2024-02-29 00:23:21 +0530

    open-save

--- open-save.js
+++ open-save.js
@@ -1,32 +1,44 @@
 let downloadBtn = document.querySelector(".download");
-let openbtn = document.querySelector(".open");
+let openBtn = document.querySelector(".open");
 
-downloadBtn.addEventListener("click", (e)=>{
-  let jsonData = JSON.stringify([sheetDb , graphComponentMatrix]);
-  let file = new Blob([jsonData], {
-    type: "application/json"
-  })
-  let a = document.createElement("a");
-  a.href = URL.createObjectURL(file);
-  a.download = "SheetData.json";
-  a.click();
+// Download Task
+downloadBtn.addEventListener("click", (e) => {
+    let jsonData = JSON.stringify([sheetDB, graphComponentMatrix]);
+    let file = new Blob([jsonData], { type: "application/json" });
+
+    let a = document.createElement("a");
+    a.href = URL.createObjectURL(file);
+    a.download = "SheetData.json";
+    a.click();
 })
 
-openbtn.addEventListener("click",()=>{
-  let input = document.createElement("input");
-  input.setAttribute("type", "file");
-  input.click();
-  input.addEventListener("change",()=>{
-    let fr = new FileReader();
-    let files = input.files[0];
-    let fileObj = files[0];
-    fr.readAsArrayBuffer(fileObj);
-    fr.addEventListener("load", (e)=>{
-      let readSheetData = JSON.parse( fr.result);
-      addSheetBtn.click();
-      sheetDB = readSheetData[0];
-      graphComponentMatrix = readSheetData[1];
+// Open task (upload)
+openBtn.addEventListener("click", (e) => {
+    // Opens file explorer
+    let input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.click();
+
+    input.addEventListener("change", (e) => {
+        let fr = new FileReader();
+        let files = input.files;
+        let fileObj = files[0];
+
+        fr.readAsText(fileObj);
+        fr.addEventListener("load", (e) => {
+            let readSheetData = JSON.parse(fr.result);
+
+            // Basic sheet with default data will be created
+            addSheetBtn.click();
+
+            // SheetDB, graphComponent
+            sheetDB = readSheetData[0];
+            graphComponentMatrix = readSheetData[1];
+
+            collectedSheetDB[collectedSheetDB.length-1] = sheetDB;
+            collectedGraphComponent[collectedGraphComponent.length-1] = graphComponentMatrix;
+
+            handleSheetProperties();
+        })
     })
-  })
-    
 })
