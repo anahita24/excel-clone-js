63693f0066589e65fc47419d27db330659460dfd
Anahita Tiwary <anahitatiwary@gmail.com>
2024-02-29 00:13:11 +0530

    pull taken

---
+++ open-save.js
@@ -0,0 +1,20 @@
+let downloadBtn = document.querySelector(".download");
+let openbtn = document.querySelector(".open");
+
+downloadBtn.addEventListener("click", (e)=>{
+  let jsonData = JSON.stringify([sheetDb , graphComponentMatrix]);
+  let file = new Blob([jsonData], {
+    type: "application/json"
+  })
+  let a = document.createElement("a");
+  a.href = URL.createObjectURL(file);
+  a.download = "SheetData.json";
+  a.click();
+})
+
+openbtn.addEventListener("click",()=>{
+  let input = document.createElement("input");
+  input.setAttribute()
+    var fr = new FileReader();
+    
+})
